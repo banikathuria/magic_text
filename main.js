d4feb045a894af7cdd142fbb1c1acc9993c03169
banikathuria <79353266+banikathuria@users.noreply.github.com>
2022-11-26 12:11:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 nosex=0
 nosey=0
+lwristx=0
+lwristy=0
+rwristx=0
+rwristy=0
+difference=0
 function preload (){}
 function setup(){
 canvas=createCanvas(450,450)
@@ -11,8 +16,9 @@ posenet.on("pose",gotResult)
 function draw(){
  background("#fe83d8")
     fill("#ad00ff")   
-    textSize(32);
+    textSize(difference);
 text('Bani', nosex, nosey);
+document.getElementById("text").innerHTML=difference
 }
 function modelLoaded(){
     console.log("Model is Loaded")
@@ -23,6 +29,12 @@ if (result.length > 0){
     console.log(result)  
     nosex=result[0].pose.nose.x
     nosey=result[0].pose.nose.y
+    lwristx=result[0].pose.leftWrist.x
+    lwristy=result[0].pose.leftWrist.y
+    rwristx=result[0].pose.rightWrist.x
+    rwristy=result[0].pose.rightWrist.y
+    difference=floor(lwristx-rwristx)
+    
 }
     
 }
